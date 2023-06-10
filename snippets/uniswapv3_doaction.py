a8decdfcad82e67e833e781f8464bd2d0c404525
CompassLabs <scheuclu@gmail.com>
2023-06-10 13:55:01 +0000

    Update from https://github.com/CompassLabs/Dojo/commit/636e181d6880824f440ecfa79fa00291f63d3b94

--- snippets/uniswapv3_doaction.py
+++ snippets/uniswapv3_doaction.py
@@ -9,10 +9,11 @@ from dojo.environments.uniswapV3 import UniV3Action, UniV3Env
 # USDC/WETH pool on UniswapV3
 pools = ["0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8"]  # WETH/USDC
 sim_start = datetime(2023, 4, 29)
-sim_end = datetime(2023, 5, 10)
+sim_end = datetime(2023, 4, 30)
 
+agent=UniV3PoolWealthAgent(initial_portfolio={"USDC": Decimal(10_000)})
 env = UniV3Env(
-    agents=[],  # Of course, you'd want an agent here to actually do things
+    agents=[agent],  # Of course, you'd want an agent here to actually do things
     date_range=(sim_start, sim_end),
     pools=pools,
     market_impact="replay",  # defaults to "replay", simply replaying history
@@ -20,12 +21,11 @@ env = UniV3Env(
 env.reset()
 
 action = UniV3Action(
-    agent=UniV3PoolWealthAgent(initial_portfolio={"USDC": Decimal(10_000)}),
+    agent=agent,
     type="quote",
     pool=env.obs.pools[0],
     quantities=np.array([Decimal(0.1), Decimal(1)]),
     tick_range=np.array([Decimal(0), Decimal(1)]),
-    owner="0xc36442b4a4522e871399cd717abdd847ab11fe88",
 )
 
 env.step(actions=[action])
