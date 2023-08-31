5acfc0eb4f5703a9dfdd02d18a14f3fe2e8f1c4d
CompassLabs <scheuclu@gmail.com>
2023-08-31 14:25:01 +0000

    Update from https://github.com/CompassLabs/Dojo/commit/c5469dc098635f03328e18a983253f49945e91ed

--- run.py
+++ run.py
@@ -13,9 +13,9 @@ from dojo.runners import backtest_run
 
 def main():
     # SNIPPET 1 START
-    pool = "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8"
+    pool = "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640"
     start_time = dateparser.parse("2023-04-29 10:00:00 UTC")
-    end_time = dateparser.parse("2023-04-30 16:00:00 UTC")
+    end_time = dateparser.parse("2023-04-29 16:00:00 UTC")
 
     demo_agent = UniV3PoolWealthAgent(
         initial_portfolio={"USDC": Decimal(10_000), "WETH": Decimal(1)}
