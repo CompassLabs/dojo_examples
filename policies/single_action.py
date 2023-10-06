f92e2dd7a1e468e7f563a6037f0c5f36d1ddcd2f
CompassLabs <scheuclu@gmail.com>
2023-10-06 16:30:48 +0000

    Update from https://github.com/CompassLabs/Dojo/commit/6bed4c5e15487f1cec356cc4dddce6528c3bbc42

---
+++ policies/single_action.py
@@ -0,0 +1,28 @@
+from typing import List
+
+from dojo.actions import BaseAction
+from dojo.agents.base_agent import BaseAgent
+from dojo.observations import BaseObs
+from dojo.policies import BasePolicy
+
+
+class SingleAction(BasePolicy):
+    """A policy that executes a single action.
+
+    This is useful for testing the impact of a single action in an environment.
+    For example, you might want to see what the final price in a UniswapV3 pool
+    would be if you executed a single swap.
+    """
+
+    def __init__(self, agent: BaseAgent, action: BaseAction) -> None:
+        """Initialize the policy.
+
+        :param agent: The agent which is using this policy.
+        :param action: The action to execute.
+        """
+        super().__init__(agent)
+        self.action = action
+
+    def predict(self, obs: BaseObs) -> List[BaseAction]:
+        """Return the action to execute."""
+        return [self.action]
