ef0fe87025f53aad6464d0df953c0471c9dba18b
CompassLabs <scheuclu@gmail.com>
2023-11-27 08:52:30 +0000

    Update from https://github.com/CompassLabs/Dojo/commit/b01edd99f86ecc9f15367831079c8c3b2a68fb7e

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,4 +1,5 @@
+from .aavev3_agent import AAVEv3Agent
 from .univ3_impermanent_loss import ImpermanentLossAgent
 from .uniV3_pool_wealth import UniV3PoolWealthAgent
 
-__all__ = ["UniV3PoolWealthAgent", "ImpermanentLossAgent"]
+__all__ = ["UniV3PoolWealthAgent", "ImpermanentLossAgent", "AAVEv3Agent"]
