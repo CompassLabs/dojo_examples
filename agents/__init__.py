721640cefc456f9a2e08df3df881a62940577272
Lukas Scheucher <scheuclu@gmail.com>
2024-06-20 10:57:31 +0100

    Update __init__.py

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,5 +1,4 @@
-from .aavev3_agent import AAVEv3Agent
 from .univ3_impermanent_loss import ImpermanentLossAgent
 from .uniV3_pool_wealth import UniV3PoolWealthAgent
 
-__all__ = ["UniV3PoolWealthAgent", "ImpermanentLossAgent", "AAVEv3Agent"]
+__all__ = ["UniV3PoolWealthAgent", "ImpermanentLossAgent"]
